88114001dc3fb19ce898304b1bbb7aae9f716d28
Ashton Mathias <mathiasashton07@gmail.com>
2024-11-08 00:02:36 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.BACKEND': JSON.stringify(env.BACKEND)
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
-  }
+  },
+  define: {
+    "process.env": {},
+  },
 })
