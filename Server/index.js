53754988e3e77bd670b4ae96e62d1c23278e2b31
Ashton Mathias <mathiasashton07@gmail.com>
2024-11-11 22:08:41 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -149,8 +149,8 @@ app.post('/api/compare', async (req, res) => {
 
         // Navigate to product URLs and wait for page load
         await Promise.all([
-            page1.goto(link1, { waitUntil: 'networkidle0' }),
-            page2.goto(link2, { waitUntil: 'networkidle0', timeout: 60000 })
+            page1.goto(link1, { waitUntil: 'networkidle2', timeout: 60000 }),
+            page2.goto(link2, { waitUntil: 'networkidle2', timeout: 60000 })
         ]);
 
         // Remove unnecessary tags and retrieve cleaned HTML content
